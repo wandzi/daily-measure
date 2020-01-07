6b03832ffa19ce1ca2951d1395bf4126d48bc322
wandzi <wandzel.mateusz@yahoo.com>
2020-01-07 07:14:39 +0100

    Updates

--- precache-manifest.171c584c743491b1a612dc4f62463b5d.js
+++ precache-manifest.171c584c743491b1a612dc4f62463b5d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cdd100cf85c7cc22135750f408a54fb9",
+    "revision": "64b081c455beb5ccfd82e023cac9e663",
     "url": "/daily-measure/index.html"
   },
   {
-    "revision": "62b6484e6a4bf518938a",
+    "revision": "2974bceea2b4bf55ce4e",
     "url": "/daily-measure/static/css/main.bc3f8efc.chunk.css"
   },
   {
-    "revision": "c8de5112e2fa07852529",
-    "url": "/daily-measure/static/js/2.854b256a.chunk.js"
+    "revision": "dceada614582c44293bd",
+    "url": "/daily-measure/static/js/2.e08ac209.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/daily-measure/static/js/2.854b256a.chunk.js.LICENSE"
+    "url": "/daily-measure/static/js/2.e08ac209.chunk.js.LICENSE"
   },
   {
-    "revision": "62b6484e6a4bf518938a",
-    "url": "/daily-measure/static/js/main.578eff49.chunk.js"
+    "revision": "2974bceea2b4bf55ce4e",
+    "url": "/daily-measure/static/js/main.61182904.chunk.js"
   },
   {
     "revision": "2b212361c39c69d57e90",
