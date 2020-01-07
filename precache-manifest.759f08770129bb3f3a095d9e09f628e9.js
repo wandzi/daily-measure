ecf022dcb7e7c783a1d142eb63a63b6335e51141
wandzi <wandzel.mateusz@yahoo.com>
2020-01-07 07:03:59 +0100

    Updates

--- precache-manifest.759f08770129bb3f3a095d9e09f628e9.js
+++ precache-manifest.759f08770129bb3f3a095d9e09f628e9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ad2cbd2666d14fb66231ad02641fcb7",
+    "revision": "4527051ba3c8dbcdf26c6776cf2efb10",
     "url": "/daily-measure/index.html"
   },
   {
-    "revision": "5b62088a9bbe81f2d423",
+    "revision": "41b5122dafdcee299c62",
     "url": "/daily-measure/static/css/main.bc3f8efc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/daily-measure/static/js/2.2814fcd7.chunk.js.LICENSE"
   },
   {
-    "revision": "5b62088a9bbe81f2d423",
-    "url": "/daily-measure/static/js/main.68dee9ac.chunk.js"
+    "revision": "41b5122dafdcee299c62",
+    "url": "/daily-measure/static/js/main.3d5325d7.chunk.js"
   },
   {
     "revision": "2b212361c39c69d57e90",
